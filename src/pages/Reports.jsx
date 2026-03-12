3ba106d45bfa68a7b95bace1daff7a64abee128d
Fábio Malveira <malveira.fabio@gmail.com>
2026-03-12 01:49:07 +0000

    File changes

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -6,6 +6,13 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import {
   Table,
   TableBody,
   TableCell,
